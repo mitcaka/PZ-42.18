1801ee1293bdc6d8df1ea8e3c93047ba7b67901f
Nguyen Mai <67093594+mitcaka@users.noreply.github.com>
2026-06-02 22:31:18 +0700

    up

--- Mods/FADED/FWP/42/media/lua/shared/NPCs/FWP_Sling_Locations.lua
+++
@@ -1,6 +0,0 @@
-require 'NPCs/BodyLocations'
-
-local bodyGroup = BodyLocations.getGroup("Human")
-FWPBodyLocation = FWPBodyLocation or {}
-FWPBodyLocation.SLING = FWPBodyLocation.SLING or ItemBodyLocation.register("fwp:FWPSling")
-bodyGroup:getOrCreateLocation(FWPBodyLocation.SLING)
